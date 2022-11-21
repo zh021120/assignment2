c8ffb9cbb7326641d201b085c98a49b2d7f223a2
zh021120 <117690327+zh021120@users.noreply.github.com>
2022-11-22 00:04:54 +0600

    Update store.py

--- assignment2_bonus/store.py
+++ assignment2_bonus/store.py
@@ -1,8 +1,8 @@
 import os
 from supabase import create_client, Client
 
-url: str = os.environ.get("SUPABASE_URL")
-key: str = os.environ.get("SUPABASE_ANON_KEY")
+url: str = os.environ.get("https://qmamdjopqczccdglqoua.supabase.co")
+key: str = os.environ.get("zhanna2002nov")
 
 supabase: Client = create_client(url, key)
 
